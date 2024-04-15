b5fcf7a022a441d8f7e154679429f12ae319fa2d
Péter Palotai <peter.palotai@stud.uni-corvinus.hu>
2024-04-15 19:04:45 +0200

    Api hívás teszt

--- kliens_alkalmazas/kliens_alkalmazas/Form1.Designer.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.Designer.cs
@@ -28,7 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -40,14 +43,30 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Rendelések kezelése";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(887, 501);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +75,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
--- kliens_alkalmazas/kliens_alkalmazas/Form1.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hotcakes.CommerceDTO.v1.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,21 @@ namespace kliens_alkalmazas
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string url = "http://20.234.113.211:8081/en-us/";
+            string key = "1-96b39a7e-b4d5-4e33-ab50-b2176bfb9844";
+            var p = new Api(url, key);
+            var s = p.CategoriesFindAll();
+            string kiirnam = s.Content.Count.ToString();
+            textBox1.Text = kiirnam;
         }
     }
 }
