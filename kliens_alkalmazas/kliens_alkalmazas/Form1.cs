22c6a850b506ffd80f5f2f80efa3feed8c8c03f1
Péter Palotai <peter.palotai@stud.uni-corvinus.hu>
2024-04-15 23:45:56 +0200

    Egyik rendelés Id behivatkozása

--- kliens_alkalmazas/kliens_alkalmazas/Form1.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.cs
@@ -35,7 +35,7 @@ namespace kliens_alkalmazas
             Api proxy = new Api(url, key);
 
             // specify the order to look for
-            var orderId = "4140928c-e2c8-4f2f-bb65-167fc912adcc";
+            var orderId = "ad66e293-9afa-4470-a233-b7c9b2bf61f0";
 
             // call the API to find the order
             ApiResponse<OrderDTO> response = proxy.OrdersFind(orderId);
