21692d968d3a48cc08a58a8fd669ed45e0976df4
Péter Palotai <peter.palotai@stud.uni-corvinus.hu>
2024-04-18 00:06:39 +0200

    a hiba elhárításával való próbálkozás

--- kliens_alkalmazas/kliens_alkalmazas/Form1.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.cs
@@ -47,7 +47,7 @@ namespace kliens_alkalmazas
             JObject jResponse = JObject.Parse(response.ObjectToJson());
             JArray jArray = (JArray)jResponse["Content"];
 
-            string[] keysToRemove = { "Password", "Addressess", "TaxExempt", "Notes", "PricingGroupId", "FailedLoginCount", "LastUpdatedUtc", "BillingAddress"};
+            string[] keysToRemove = { "Password", "Addressess", "TaxExempt", "Notes", "PricingGroupId", "FailedLoginCount", "LastUpdatedUtc", "ShippingAddress", "Bvin", "LastUpdatedUtc", "StoreId", "NickName", "FirstName", "MiddleInitial", "LastName", "Company", "Line1", "Line2", "Line3", "City", "RegionName", "RegionBvin", "PostalCode", "CountryName", "CountryBvin", "Phone", "Fax", "WebSiteUrl", "CountyName", "CountyBvin", "UserBvin", "AddressType", "BillingAddress", "Bvin", "LastUpdatedUtc", "StoreId", "NickName", "FirstName", "MiddleInitial", "LastName", "Company", "Line1", "Line2", "Line3", "City", "RegionName", "RegionBvin", "PostalCode", "CountryName", "CountryBvin", "Phone", "Fax", "WebSiteUrl", "CountyName", "CountyBvin", "UserBvin", "AddressType" };
             foreach (JObject felhasznalo in jArray)
             {
                 foreach (var keys in keysToRemove.ToList())
