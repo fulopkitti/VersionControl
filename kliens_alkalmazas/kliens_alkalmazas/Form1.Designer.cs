cfb6052cb4920fcd5bfe5b08625fab89d81ea3ec
Péter Palotai <peter.palotai@stud.uni-corvinus.hu>
2024-04-18 11:58:01 +0200

    Az időpont szerinti szűrés megoldódott.

--- kliens_alkalmazas/kliens_alkalmazas/Form1.Designer.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.Designer.cs
@@ -34,13 +34,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(828, 419);
+            this.label1.Location = new System.Drawing.Point(827, 458);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(194, 16);
             this.label1.TabIndex = 0;
@@ -48,7 +53,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(831, 438);
+            this.textBox1.Location = new System.Drawing.Point(830, 477);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(166, 22);
             this.textBox1.TabIndex = 1;
@@ -60,7 +65,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1055, 341);
+            this.dataGridView1.Size = new System.Drawing.Size(1055, 356);
             this.dataGridView1.TabIndex = 2;
             // 
             // label2
@@ -84,7 +89,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(26, 468);
+            this.button2.Location = new System.Drawing.Point(25, 477);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(156, 23);
             this.button2.TabIndex = 7;
@@ -92,11 +97,57 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(261, 478);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(502, 478);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 425);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Utolsó bejelentkezés időpontja:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Kezdő dátum";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(499, 459);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Záró dátum";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1102, 562);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -120,6 +171,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- kliens_alkalmazas/kliens_alkalmazas/Form1.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.cs
@@ -27,20 +27,18 @@ namespace kliens_alkalmazas
         {
             InitializeComponent();
         }
-
-        
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private static Api apiHivas()
         {
             string url = "http://20.234.113.211:8081";
             string kulcs = "1-96b39a7e-b4d5-4e33-ab50-b2176bfb9844";
 
             Api proxy = new Api(url, kulcs);
+            return proxy;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Api proxy = apiHivas();
             var s = proxy.CustomerAccountsCountOfAll().Content;
             string kiirom = s.ToString();
             textBox1.Text = kiirom;
@@ -65,15 +63,52 @@ namespace kliens_alkalmazas
             dataGridView1.DataSource = userTabla;
         }
 
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Api proxy = apiHivas();
+            var s = proxy.CustomerAccountsCountOfAll().Content;
+            string kiirom = s.ToString();
+            textBox1.Text = kiirom;
+
+
+
+            var response = proxy.CustomerAccountsFindAll();
+
+            JObject jResponse = JObject.Parse(response.ObjectToJson());
+            JArray jArray = (JArray)jResponse["Content"];
+
+            string[] keysToRemove = { "Password", "Addresses", "Notes", "TaxExempt", "PricingGroupId", "FailedLoginCount", "LastUpdatedUtc", "ShippingAddress", "BillingAddress" };
+            foreach (JObject felhasznalo in jArray)
+            {
+                foreach (var key in keysToRemove.ToList())
+                {
+                    felhasznalo.Remove(key);
+                }
+            }
+
+            DataTable userTabla = (DataTable)JsonConvert.DeserializeObject(jArray.ToString(), typeof(DataTable));
+
+            
+            DateTime fromDate = dateTimePicker1.Value.Date;
+            DateTime toDate = dateTimePicker2.Value.Date.AddDays(1).AddTicks(-1);
+
+            DataView dv = userTabla.DefaultView;
+            dv.RowFilter = $"LastLoginDateUtc >= '{fromDate}' AND LastLoginDateUtc <= '{toDate}'";
+            DataTable filteredTable = dv.ToTable();
+
+            dataGridView1.DataSource = filteredTable;
+        }
+
+        
+
         private void button2_Click(object sender, EventArgs e)
         {
             authentication azon = new authentication();
             if (azon.ShowDialog() != DialogResult.OK) return;
 
-            string url = "http://20.234.113.211:8081";
-            string kulcs = "1-96b39a7e-b4d5-4e33-ab50-b2176bfb9844";
-
-            Api proxy = new Api(url, kulcs);
+            Api proxy = apiHivas();
             var customerID = azon.textBox3.Text;
             ApiResponse<bool> response = proxy.CustomerAccountsDelete(customerID);
 
