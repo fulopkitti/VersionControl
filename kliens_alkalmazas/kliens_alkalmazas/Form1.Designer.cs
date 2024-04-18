302cf921ea0024d3d1ae545db90bdc4b8ef8597e
Péter Palotai <peter.palotai@stud.uni-corvinus.hu>
2024-04-18 09:46:28 +0200

    a formra felkerült a jelenleg aktív felhasználók megjelenítése és egy "Adatok betöltése" button melyel lehet frissíteni majd a megjleneített adatokat

--- kliens_alkalmazas/kliens_alkalmazas/Form1.Designer.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.Designer.cs
@@ -31,21 +31,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(827, 435);
+            this.label1.Location = new System.Drawing.Point(828, 431);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 16);
+            this.label1.Size = new System.Drawing.Size(194, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Regisztrált felhasználók száma";
+            this.label1.Text = "Regisztrált felhasználók száma:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(830, 454);
+            this.textBox1.Location = new System.Drawing.Point(831, 450);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(166, 22);
             this.textBox1.TabIndex = 1;
@@ -53,18 +57,56 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(35, 36);
+            this.dataGridView1.Location = new System.Drawing.Point(26, 42);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(983, 318);
+            this.dataGridView1.Size = new System.Drawing.Size(1055, 341);
             this.dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Regisztrált felhasználóink adatai:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(828, 488);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Aktív felhasználók száma:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(831, 507);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 507);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Adatok frissítése";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1048, 531);
+            this.ClientSize = new System.Drawing.Size(1102, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -82,6 +124,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
