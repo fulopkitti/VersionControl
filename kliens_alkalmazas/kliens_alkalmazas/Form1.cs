58bcf98700f3fb591b4fd0b1377b3448f4400d3f
Péter Palotai <peter.palotai@stud.uni-corvinus.hu>
2024-04-18 08:31:27 +0200

    az össszes felhasználó megjelenése mkődik, szűrt columnokkal

--- kliens_alkalmazas/kliens_alkalmazas/Form1.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.cs
@@ -33,9 +33,9 @@ namespace kliens_alkalmazas
         private void Form1_Load(object sender, EventArgs e)
         {
             string url = "http://20.234.113.211:8081";
-            string key = "1-96b39a7e-b4d5-4e33-ab50-b2176bfb9844";
+            string kulcs = "1-96b39a7e-b4d5-4e33-ab50-b2176bfb9844";
 
-            Api proxy = new Api(url, key);
+            Api proxy = new Api(url, kulcs);
             var s = proxy.CustomerAccountsCountOfAll().Content;
             string kiirom = s.ToString();
             textBox1.Text = kiirom;
@@ -50,7 +50,7 @@ namespace kliens_alkalmazas
             string[] keysToRemove = { "Password", "Addressess", "TaxExempt", "Notes", "PricingGroupId", "FailedLoginCount", "LastUpdatedUtc", "ShippingAddress", "Bvin", "LastUpdatedUtc", "StoreId", "NickName", "FirstName", "MiddleInitial", "LastName", "Company", "Line1", "Line2", "Line3", "City", "RegionName", "RegionBvin", "PostalCode", "CountryName", "CountryBvin", "Phone", "Fax", "WebSiteUrl", "CountyName", "CountyBvin", "UserBvin", "AddressType", "BillingAddress", "Bvin", "LastUpdatedUtc", "StoreId", "NickName", "FirstName", "MiddleInitial", "LastName", "Company", "Line1", "Line2", "Line3", "City", "RegionName", "RegionBvin", "PostalCode", "CountryName", "CountryBvin", "Phone", "Fax", "WebSiteUrl", "CountyName", "CountyBvin", "UserBvin", "AddressType" };
             foreach (JObject felhasznalo in jArray)
             {
-                foreach (var keys in keysToRemove.ToList())
+                foreach (var key in keysToRemove.ToList())
                 {
                     felhasznalo.Remove(key);
                 }
