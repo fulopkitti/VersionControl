1dbc29d7f57b4ce489de8432728a959bff42b0b8
peterpalotai <150239753+peterpalotai@users.noreply.github.com>
2024-04-23 13:10:18 +0200

    test

---
+++ kliens_alkalmazas/UnitTestProject_authentication/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject_authentication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject_authentication")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("48c42c0f-76a2-495e-97e8-9ad876f08834")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ kliens_alkalmazas/UnitTestProject_authentication/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using kliens_alkalmazas;
+
+namespace UnitTestProject_authentication
+{
+    [TestMethod]
+    public void TestCheckUser_ValidUsername_ReturnsTrue()
+    {
+        string validUsername = "admin";
+
+        bool result = authentication.CheckUser(validUsername);
+
+
+        Assert.IsTrue(result);
+    }
+
+    public void TestCheckUser_InvalidUsername_ReturnsFalse()
+    {
+        string invalidUsername = "user";
+
+
+        bool result = authentication.CheckUser(invalidUsername);
+
+
+        Assert.IsFalse(result);
+    }
+
+    public void TestCheckPassword_ValidPassword_ReturnsTrue()
+    {
+        string validPassword = "asd123";
+
+        bool result = authentication.CheckPassword(validPassword);
+
+        Assert.IsTrue(result);
+    }
+
+    public void TestCheckPassword_InvalidPassword_ReturnsFalse()
+    {
+        string invalidPassword = "password";
+
+        bool result = authentication.CheckPassword(invalidPassword);
+
+        Assert.IsFalse(result);
+    }
+
+    public void TestCheckId_ValidId_ReturnsTrue()
+    {
+        int validId = 5;
+
+        bool result = authentication.CheckId(validId);
+
+        Assert.IsTrue(result);
+    }
+
+    public void TestCheckId_InvalidId_ReturnsFalse()
+    {
+        int invalidId = 10;
+
+        bool result = authentication.CheckId(invalidId);
+
+        Assert.IsFalse(result);
+    }
+}
+
